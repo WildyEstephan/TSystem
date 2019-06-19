ac7aa1e037491da941b4094973b095e541196332
Wildy Estephan <westephan50@gmail.com>
2019-06-19 14:58:13 -0400

    Update student.py

--- tropi_school/models/student.py
+++ tropi_school/models/student.py
@@ -13,18 +13,3 @@ class Student(models.Model):
                                  string="Section", required=False, domain="[('grade_id', '=', grade_id)]")
     scholarship_id = fields.Many2one(comodel_name="tropi.school.scholarship",
                                      string="Scholarship", required=False, )
-
-
-    @api.model
-    def create(self, values):
-        # Add code here
-
-        if values['student'] and values['ground_id']:
-
-            ground = self.env['tropi.school.ground'].search([('id', '=', values['ground_id'])])[0]
-
-            values['id_student'] = ground.code + self.env['ir.sequence'].next_by_code('seq.id.student')
-
-
-
-        return super(Student, self).create(values)
