7701d28b9d6c0abcac2d2e0cd4df9dd4e12ee5ef
Wildy Estephan <westephan50@gmail.com>
2020-05-29 17:13:05 -0400

    Update __manifest__.py

--- todo_app/__manifest__.py
+++ todo_app/__manifest__.py
@@ -5,7 +5,7 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
-        'security/todo_access_rules',
+        'security/todo_access_rules.xml',
         'views/todo_menu.xml',
         'views/todo_view.xml',
         'views/res_partner_view.xml',
