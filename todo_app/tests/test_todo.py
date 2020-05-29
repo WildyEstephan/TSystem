88a60aee10899f72f9851ff23d42f4ddfd5fa5da
Wildy Estephan <westephan50@gmail.com>
2020-05-29 16:23:25 -0400

    Update test_todo.py

--- todo_app/tests/test_todo.py
+++ todo_app/tests/test_todo.py
@@ -7,3 +7,10 @@ class TestTodo(TransactionCase):
         Todo = self.env['todo.task']
         task = Todo.create({'name': 'Test Task'})
         self.assertEqual(task.is_done, False)
+
+    def test_clear_done(self):
+        "Clear Done sets Todo to not active"
+        Todo = self.env['todo.task']
+        task = Todo.create({'name': 'Test Dark'})
+        task.do_clear_done()
+        self.asserFalse(task.active)
